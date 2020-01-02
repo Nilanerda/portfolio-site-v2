bdb1e6e19c8d67a14635410df7671367ce2b7b2a
Yana <zzizzu@ukr.net>
2020-01-02 13:41:28 +0200

    create preloader.js file and replace preloader logic from app.js, delete preloader hidding styles, add import in index.js

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,8 +1,9 @@
-import 'jquery'
+import 'jquery';
 import 'normalize.css';
 import '../sass/fonts.scss';
 import './app/app';
-import './app/slick-carousel'
+import './app/preloader.js';
+import './app/slick-carousel';
 import 'slick-carousel/slick/slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
